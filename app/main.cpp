4d7e5751c49f2516b99c2ab0bf4cb4c9dea6f343
Rasmus <rasmus.kallqvist@gmail.com>
2019-07-14 12:59:23 +0200

    Add application of InterruptManager to main
    Led is blinked by using timer1 compare match interrupt to trigger
    a GpioPin toggle. Interrupt is owned by an InterruptManager instance.

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,14 @@
 #include "gpiopin.h"
 #include "timer1.h"
+#include "interruptmanager.h"
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
 static GpioPin ledPin = GpioPin(PIN_5, PORT_B, DIGITAL_OUTPUT);
+static InterruptManager interruptManager;
 static Timer1 tim1;
 
 void init();
-
-ISR(TIMER1_COMPA_vect)
-{
-	ledPin.toggle();
-}
 
 int main()
 {
@@ -25,10 +22,12 @@ int main()
 
 void init()
 {
-	// enable global interrupts
-	sei();
+	interruptManager.enableInterruptsGlobally();
 
-	// setup timer1
+	InterruptHandler interruptHandler = [](){ ledPin.toggle(); };
+	InterruptRequest interruptRequest = InterruptRequest::TIMER1_COMPA;
+	interruptManager.setHandlerForInterrupt(interruptHandler, interruptRequest);
+
 	tim1.enablePeriodicInterrupts();
 	tim1.setPrescaler(Timer16Bit::PrescaleOption::_1);
 	tim1.setPeriod(160);
