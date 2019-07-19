df899e1256bee0e57b07aa463d2b123ab677d56e
Warwolt <rasmus.kallqvist@gmail.com>
2019-07-19 15:39:38 +0200

    Add application of TempoTimer16Bit to main

--- app/main.cpp
+++ app/main.cpp
@@ -1,37 +1,57 @@
 #include "gpiopin.h"
 #include "timer1.h"
 #include "interrupts.h"
-#include <avr/interrupt.h>
-#include <avr/io.h>
-#include "fixmath/fixmath.h"
+#include "tempotimer16bit.h"
 
 static GpioPin ledPin = GpioPin(Pin5, PortB, DigitalOutput);
 static Timer1 tim1;
+static TempoTimer16Bit tempoTimer = TempoTimer16Bit(tim1);
 
 void init();
+void registerTimerInterrupt();
+void setupTimer1();
+void setupTempoTimer();
 
 int main()
 {
 	init();
 
-	Fix16 num = fix16_from_int(5);
-
 	while(1)
 	{
-		num++;
+		if(tempoTimer.playbackStepIsDue())
+		{
+			ledPin.toggle();
+			tempoTimer.startCountingNextStep();
+		}
 	}
 }
 
 void init()
 {
 	Interrupts::enableInterruptsGlobally();
+	registerTimerInterrupt();
+	setupTimer1();
+	setupTempoTimer();
+}
 
-	InterruptHandler interruptHandler = []{ ledPin.toggle(); };
-	InterruptRequest interruptRequest = InterruptRequest::Timer1CompareMatch;
-	Interrupts::setHandlerForInterrupt(interruptHandler, interruptRequest);
+void registerTimerInterrupt()
+{
+	InterruptHandler timerISR = []
+	{
+		tempoTimer.countPulse();
+	};
+	InterruptRequest timerIRQ = InterruptRequest::Timer1CompareMatch;
+	Interrupts::setHandlerForInterrupt(timerISR, timerIRQ);
+}
 
+void setupTimer1()
+{
 	tim1.enablePeriodicInterrupts();
 	tim1.setPrescaler(Timer16Bit::PrescaleOption::_1);
-	tim1.setPeriod(160);
-	tim1.start();
+}
+
+void setupTempoTimer()
+{
+	tempoTimer.setTempo(BeatsPerMinute(120));
+	tempoTimer.start();
 }
