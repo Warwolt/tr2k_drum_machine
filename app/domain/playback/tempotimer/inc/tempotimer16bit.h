45b94ead767475f52d949052abdb70ef6cba2aff
Warwolt <rasmus.kallqvist@gmail.com>
2019-07-19 14:50:23 +0200

    Add TempoTimer interface that TempoTimer16Bit implements

--- app/domain/playback/tempotimer/inc/tempotimer16bit.h
+++ app/domain/playback/tempotimer/inc/tempotimer16bit.h
@@ -8,10 +8,10 @@
 #define TEMPO_TIMER_16_BIT_H
 
 #include "linuxtypes.h"
+#include "tempotimer.h"
 #include "timer16bit.h"
-#include "beatsperminute.h"
 
-class TempoTimer16Bit
+class TempoTimer16Bit : TempoTimer
 {
 public:
 	TempoTimer16Bit(Timer16Bit& timer16Bit);
@@ -24,7 +24,7 @@ public:
 	void resetPlaybackStepDue();
 
 private:
-	static constexpr u32 clockFrequency = 16000000;
+	static constexpr u32 clockFrequency = 16e6;
 	static constexpr u8  secondsPerMinute = 60;
 	static constexpr u16 pulsesPerQuarterNote = 480;
 	static constexpr u16 pulsesPerSixteenthNote = pulsesPerQuarterNote / 4;
