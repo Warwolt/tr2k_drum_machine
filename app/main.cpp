64c9307d84fbdadfee2fe65c0f04a855f6f68df0
Rasmus <rasmus.kallqvist@gmail.com>
2019-08-06 19:34:22 +0200

    Add prototype code for 74hc595 based segment display

--- app/main.cpp
+++ app/main.cpp
@@ -11,7 +11,15 @@ static Timer1 tim1;
 static TempoTimer16Bit tempoTimer = TempoTimer16Bit(tim1);
 static TempoTimingManager timingManager = TempoTimingManager(tempoTimer);
 static GpioPin ledPin = GpioPin(Pin5, PortC, DigitalOutput);
+static GpioPin dataLatchPin = GpioPin(Pin2, PortB, DigitalOutput);
 
+static unsigned char segmentDataLut[] =
+{// 0	 1	  2	   3	4	 5	  6	   7	8	 9	  A	   b	C    d	  E    F    -
+	0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90,0x8C,0xBF,0xC6,0xA1,0x86,0xFF,0xbf
+};
+
+void displayNumber(u16 number);
+void displayDigit(u8 segmentData, u8 digitData);
 void init();
 void registerSpiInterrupt();
 void registerTimerInterrupt();
@@ -23,23 +31,38 @@ int main()
 {
 	init();
 
-	r2k::vector<u8, 4> txBuffer = {1, 2, 3, 4};
-
 	while (1)
 	{
+		displayNumber(1337);
+		_delay_us(2000);
+	}
+}
+
+void displayNumber(u16 num)
+{
+	u16 digits[] = {num % 10, num/10 % 10, num/100 % 10, num/1000 % 10};
+	for(int i = 0; i < 4; i++)
+	{
+		displayDigit(segmentDataLut[digits[i]], i);
+		_delay_us(400);
+	}
+	displayDigit(0xFF, 5);
+}
+
+void displayDigit(u8 segmentData, u8 digitNum)
+{
+		r2k::vector<u8, 2> txBuffer = {segmentData, static_cast<u8>(0x1 << digitNum)};
 		spi.setTxBuffer(txBuffer);
 		spi.sendNextByteInBuffer();
-		_delay_us(100);
-	}
 }
 
 void init()
 {
 	Interrupts::enableInterruptsGlobally();
-	registerSpiInterrupt();
 	registerTimerInterrupt();
-	setupSpi();
 	setupTempoTimer();
+	registerSpiInterrupt();
+	setupSpi();
 	setupTimingManager();
 }
 
@@ -50,6 +73,11 @@ void registerSpiInterrupt()
 		if (!spi.txBufferIsEmpty())
 		{
 			spi.sendNextByteInBuffer();
+		}
+		else
+		{
+			dataLatchPin.set();
+			dataLatchPin.clear();
 		}
 	};
 
@@ -69,8 +97,8 @@ void registerTimerInterrupt()
 
 void setupSpi()
 {
-	spi.setBitOrder(SpiBitOrder::LsbFirst);
-	spi.setClockSpeed(SpiClockSpeed::SysFreq_over_2);
+	spi.setBitOrder(SpiBitOrder::MsbFirst);
+	spi.setClockSpeed(SpiClockSpeed::SysFreq_over_128);
 }
 
 void setupTempoTimer()
