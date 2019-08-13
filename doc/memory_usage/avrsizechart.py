e2a7316f0358b6d4711df5f0d600e13e4f31f4b3
Warwolt <rasmus.kallqvist@gmail.com>
2019-08-13 13:11:56 +0200

    Add boot loader size to avrsizechart script

--- doc/memory_usage/avrsizechart.py
+++ doc/memory_usage/avrsizechart.py
@@ -14,12 +14,14 @@ def main():
 
 	if args.print:
 		print(avrsize_output)
+		if args.show_bootloader:
+			print("    512       0       0     512     200 atmega328 bootloader", end="")
 		return
 
 	if args.total_size:
-		create_total_size_plot(avr_row_dicts)
+		create_total_size_plot(avr_row_dicts, args.show_bootloader)
 	else:
-		create_file_size_plot(avr_row_dicts)
+		create_file_size_plot(avr_row_dicts, args.show_bootloader)
 
 	plt.tight_layout(pad=4)
 
@@ -33,10 +35,14 @@ def create_parser():
 	""" Create a CLI front-end and return it. """
 	parser = argparse.ArgumentParser(description="Creates a pie chart with data collected from \
 		the avr-size CLI tool and presents it using matplotlib.")
-	parser.add_argument("-t","--total-size", action="store_true",
-		help="display how much total memory has been used up.")
-	parser.add_argument("-p", "--print", action="store_true",
-		help="print output from avr-size to terminal instead of creating pie chart")
+	total_size_msg = "Display how much total memory has been used up."
+	print_msg = "Print output from avr-size to terminal instead of creating pie chart"
+	show_bootloader_msg = "Add bootloader size to memory usage. If option not used, maximum \
+		memory will be set to 32KB minus the bootloader size for the total size of available \
+		program memory."
+	parser.add_argument("-t","--total-size", action="store_true", help=total_size_msg)
+	parser.add_argument("-p", "--print", action="store_true", help=print_msg)
+	parser.add_argument("-b", "--show-bootloader", action="store_true", help=show_bootloader_msg)
 	parser.add_argument("-o", "--output", nargs=1)
 	return parser
 
@@ -70,14 +76,22 @@ def get_row_dicts_from_avrsize_output(avrsize_output):
 	return dicts
 
 
-def create_file_size_plot(avr_row_dicts):
+def create_file_size_plot(avr_row_dicts, show_bootloader):
 	"""
 	Take a list of dicts containing program size information and creates a matplotlib pie chart
 	that presents how big each object file is.
+
+	If show_bootloader is True, then the bootloader will be shown as a pie slice.
 	"""
 	labels = [row_dict["filename"].rstrip(".o") for row_dict in avr_row_dicts]
 	sizes  = [row_dict["dec"] for row_dict in avr_row_dicts]
 
+	# Add bootloader stats
+	if show_bootloader:
+		labels.append("bootloader")
+		sizes.append(512)
+
+	# Create plot
 	fig, ax = plt.subplots()
 	fig.subplots_adjust(wspace=1.1, right=0.9)
 	fig.canvas.set_window_title("Output of avr-size")
@@ -86,15 +100,25 @@ def create_file_size_plot(avr_row_dicts):
 	ax.set_title("Memory usage")
 
 
-def create_total_size_plot(avr_row_dicts):
+def create_total_size_plot(avr_row_dicts, show_bootloader):
 	"""
 	Take a list of dicts containing program size information and creates a matplotlib pie chart
 	that presents how much of the total program size has been used up.
+
+	If show_bootloader is True, then the bootloader will be counted into the used size. If it is
+	False, then instead the total available program memory will exclude the bootloader.
 	"""
-	atmega328_prog_memory_size = int(32e3) # 32KB
+	bootloader_size = 512
+	atmega328_prog_memory_size = int(32*1024) # 32KB
 	labels = ["used memory", "free memory"]
 	used_size  = sum([row_dict["dec"] for row_dict in avr_row_dicts])
 	unused_size = atmega328_prog_memory_size - used_size
+
+	if show_bootloader:
+		used_size += bootloader_size
+	else:
+		unused_size -= bootloader_size
+
 	sizes = [used_size, unused_size]
 
 	fig, ax = plt.subplots()
