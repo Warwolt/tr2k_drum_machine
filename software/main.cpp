edf4a0adc431287fb7dbf7cc9638b0f7916f62c0
Rasmus Källvist <rasmus.kallqvist@gmail.com>
2019-10-22 15:02:47 +0200

    Add documentation to main.cpp

--- software/main.cpp
+++ software/main.cpp
@@ -1,6 +1,10 @@
 /*
 ****************************************************************************************************
 * brief : Main program file for "tr2k drum machine" step sequencer software.
+*
+* This current version of the sequencer contains a user interface for setting the playback tempo in
+* beats per minute using a rotary encoder and viewing the set tempo on a 4-digit display. An LED is
+* then pulsed once every 16th step, to demonstrate that the tempo timing manager works.
 ****************************************************************************************************
 */
 
@@ -11,11 +15,19 @@ int main()
 	TempoControlView& tempoControlView = Startup::getTempoControlView();
 	TempoTimingManager& tempoTimingManager = Startup::getTempoTimingManager();
 
+	/* Initialize the tempo control view and the tempo timing manager, along
+	 * with all of their dependencies. */
 	Startup::init();
 
 	while(1)
 	{
+		/* Update view that consists of a 4-digit display showing the current
+		 * tempo in beats per minute, and a rotary encoder to set the tempo.*/
 		tempoControlView.handleTempoControl();
+
+		/* Manager checks with the tempo timer if next playback step is due
+		 * (16th note), and if it is due the manager calls all the playback
+		 * handler functions registered in the Startup::init() function. */
 		tempoTimingManager.handlePlayback();
 	}
 }
