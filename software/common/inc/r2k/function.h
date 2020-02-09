809561558d764c2894993d9f8cb1857aeabae14b
Rasmus <rasmus.kallqvist@gmail.com>
2020-02-09 14:57:23 +0100

    Make traceprint omit file path

--- software/common/inc/r2k/function.h
+++ software/common/inc/r2k/function.h
@@ -8,13 +8,15 @@
 #ifndef R2K_FUNCTION_H
 #define R2K_FUNCTION_H
 
+#include "traceprint.h"
+
 namespace r2k
 {
 template <typename>
 class function;
 
 /**
- * @brief Wraps some callable type, akin to a function pointer
+ * @brief Wraps some wrapped_function type, akin to a function pointer
  *
  * This is an implementation similar to std::function, that works like a
  * smart function pointer, and allows wrapping lambdas and closures.
@@ -27,13 +29,36 @@ class function<ReturnType(Args...)>
 {
 public:
 	/**
-	 * @brief Constructor
+	 * @brief Uninitalized constructor
+	 *
+	 * Used to declare r2k::function<> members in other classes
+	 */
+	function<ReturnType(Args...)>() : wrapped_function(nullptr)
+	{
+	}
+
+	/**
+	 * @brief Assignment constructor
+	 *
+	 * The primary constructor that should be used, wraps some wrapped_function type
+	 * so that it can later be called by a client.
+	 *
 	 * @param f  An instance of some function type
 	 */
 	template <typename F>
-	function<ReturnType(Args...)>(F f) : callable(new Callable<F>(f))
+	function<ReturnType(Args...)>(F f) : wrapped_function(new Callable<F>(f))
 	{
+	}
 
+	/**
+	 * @brief Copy constructor
+	 *
+	 * Needed in order to make sure that function wrappers assigned to other
+	 * function wrappers are deep copied properly.
+	 */
+	function<ReturnType(Args...)>(const function<ReturnType(Args...)>& other)
+	{
+		wrapped_function = wrapped_function->clone();
 	}
 
 	/**
@@ -41,9 +66,10 @@ public:
 	 */
 	~function<ReturnType(Args...)>()
 	{
-		if (callable)
+		if (wrapped_function)
 		{
-			delete callable;
+			INFO_PRINT("deleting wrapped_function = %p", wrapped_function);
+			delete wrapped_function;
 		}
 	}
 
@@ -54,26 +80,35 @@ public:
 	template <typename F>
 	function& operator=(F f)
 	{
-		if (callable)
+		if (wrapped_function)
 		{
-			delete callable;
+			delete wrapped_function;
 		}
-		callable = new Callable<F>(f);
+		wrapped_function = new Callable<F>(f);
 		return *this;
 	}
 
 	/**
 	 * @brief Paranthesis operator to call the wrapped function
-	 * @param args  Any needed arguments needed for the function
+	 * @param args  Any needed arguments for the function
 	 */
 	ReturnType operator()(Args... args) const
 	{
-		return callable->invoke(args...);
+		INFO_PRINT("function<ReturnType(Args...)>::operator() called");
+		if (wrapped_function)
+		{
+			INFO_PRINT("wrapped_function = %p", wrapped_function);
+			INFO_PRINT("wrapped_function->invoke(args...)");
+			return wrapped_function->invoke(args...);
+		}
 	}
 
 private:
+	class ICallable;
+	ICallable *wrapped_function;
+
 	/**
-	 * @brief Interface for wrapping any callable type
+	 * @brief Interface for wrapping any wrapped_function type
 	 *
 	 * In order to be able to store free functions, lambdas, closures and
 	 * member functions we introduce a wrapper with a vtable.
@@ -83,11 +118,12 @@ private:
 	public:
 		virtual ~ICallable() = default;
 		virtual ReturnType invoke(Args...) = 0;
+		virtual ICallable *clone() = 0;
 	};
 
 	/**
-	 * @brief Templated concrete instance of the callable wrapper
-	 * @param f  An instance of a callable type to wrap
+	 * @brief Templated concrete instance of the wrapped_function wrapper
+	 * @param f  An instance of a wrapped_function type to wrap
 	 */
 	template <typename F>
 	class Callable : public ICallable
@@ -102,13 +138,22 @@ private:
 		 */
 		ReturnType invoke(Args... args) override final
 		{
+			INFO_PRINT("Callable::invoke()");
+			INFO_PRINT("f = %p", f); //
 			return f(args...);
+		}
+
+		/**
+		 * @brief
+		 * @param other  asdf
+		 */
+		ICallable *clone()
+		{
+			return new Callable(f);
 		}
 	private:
 		F f;
 	};
-
-	ICallable *callable;
 };
 } /* namespace r2k */
 
--- software/common/inc/traceprint.h
+++ software/common/inc/traceprint.h
@@ -1,6 +1,8 @@
 /*
 ****************************************************************************************************
-* brief :  Trace printouts, for use in unit testing. Formatting is matching that of gtest prints
+* @brief :  Trace printouts, for use in unit testing. Formatting is matching that of gtest prints
+*
+* __FILE_NAME__ macro based on https://blog.galowicz.de/2016/02/20/short_file_macro/
 ****************************************************************************************************
 */
 
@@ -8,9 +10,25 @@
 #define TEST_PRINT_H
 
 #include <stdio.h>
-#define TRACE_PRINT() printf("[ TRACE    ] %s:%d\n", __FILE__, __LINE__)
+using cstr = const char * const;
+
+static constexpr cstr past_last_slash(cstr str, cstr last_slash)
+{
+    return
+        *str == '\0' ? last_slash :
+        *str == '/'  ? past_last_slash(str + 1, str + 1) :
+                       past_last_slash(str + 1, last_slash);
+}
+
+static constexpr cstr past_last_slash(cstr str)
+{
+    return past_last_slash(str, str);
+}
+
+#define __FILE_NAME__ ({constexpr cstr sf__ {past_last_slash(__FILE__)}; sf__;})
+#define TRACE_PRINT() printf("[ TRACE    ] %s:%d\n", __FILE_NAME__, __LINE__)
 #define INFO(x) "[   INFO   ] " x
-#define INFO_PRINT(...) printf("[   INFO   ] %s:%d ", __FILE__, __LINE__); printf(__VA_ARGS__); printf("\n");
+#define INFO_PRINT(...) printf("[   INFO   ] %s:%d ", __FILE_NAME__, __LINE__); printf(__VA_ARGS__); printf("\n");
 #define ERROR_MSG_STR(x) "\n\tINFO: " << x << "\n"
 
 #endif /* TEST_PRINT_H */
